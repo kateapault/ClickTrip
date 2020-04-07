06d779770f13d4ce0e3b0ae5dcf6e8b1401becf5
github username <github email address>
2020-04-07 13:13:48 -0400

    saving flight api call results to session

--- frontent-clicktrip/src/FlightSelection.js
+++ frontent-clicktrip/src/FlightSelection.js
@@ -9,10 +9,13 @@ class FlightSelection extends React.Component {
     })
     .then(resp => resp.json())
     .then( flights => {
-      let flightsHash = flights.data;
-      let flightsArray = Object.keys(flightsHash)
-      console.log(`flightsArray: ${flightsArray}`)
-      this.props.addFlights(flights.data);
+      // let flightsHash = flights.data;
+      // let flightsDest = Object.keys(flightsHash)
+      // let flightsArr = []
+      // for(let i=0;i<flightsDest.length;i++) {
+      //   flightsArr.push(flightsHash[flightsDest[i]])
+      // }
+      window.sessionStorage.setItem('flights',JSON.stringify(flights.data))
     })
   }
   render () {
@@ -20,8 +23,8 @@ class FlightSelection extends React.Component {
 
       <div>
         I'm the flight selection container. Here are my flights.
-        {console.log('flights: ')}
-        {console.log(this.props.flights)}
+        {/* {console.log('flights: ')}
+        {console.log(this.props.flights)} */}
         {this.props.flights.length > 0 ? this.props.flights.map(flight => <FlightItem flight={flight} />) : 'no flights yet'}
       </div>
     );
