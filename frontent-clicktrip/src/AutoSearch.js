f565b2328df4cbde116bdb3074968ed18272b6da
github username <github email address>
2020-04-15 21:01:39 -0400

    auto search basic formatting complete

--- frontent-clicktrip/src/AutoSearch.js
+++ frontent-clicktrip/src/AutoSearch.js
@@ -20,14 +20,16 @@ class AutoSearch extends React.Component {
     render() {
         return (
             <div className="auto-search search">
-                <form onSubmit={this.props.handleSubmit} className="search">
-                    <div>I want a vaction for <input type="number" name="num_people" placeholder="2" /> people
+                <h3>Instant Trip</h3>
+                <form className="auto-search search" onSubmit={this.props.handleSubmit} className="search">
+                    <div></div>
+                    <div className="form-input">I want a vaction for <input type="number" name="num_people" placeholder="2" /> people
                     </div>
-                    <div>for under $<input type="number" name="budget" placeholder="1200" />
+                    <div className="form-input">for under $<input type="number" name="budget" placeholder="1200" />
                     </div>
-                    <div>from <input type="date" name="start_date" /> to <input type="date" name="end_date" />
+                    <div className="form-input">from <input type="date" name="start_date" /> to <input type="date" name="end_date" />
                     </div>
-                    <div>flying out of <input type="text" name="origin_city_iata" placeholder="NYC" />
+                    <div className="form-input">flying out of <input type="text" name="origin_city_iata" placeholder="NYC" />
                     </div>
                     <button>Take me away!</button>
                 </form>
