61e7097ded59fe82142dc38f7cd0cb69927d8f09
github username <github email address>
2020-04-15 21:05:39 -0400

    search by destination and search by price basic formatting complete

--- frontent-clicktrip/src/DestinationSearch.js
+++ frontent-clicktrip/src/DestinationSearch.js
@@ -15,14 +15,15 @@ class DestinationSearch extends React.Component {
     return (
 
       <div className="destination-search search">
-        <form onSubmit={this.props.handleSubmit}>
-          I want a vaction to <input type="text" name="origin_city_iata" placeholder="LON" />
-          <br></br>
-          for <input type="number" name="num_people" placeholder="2" /> people
-          <br></br>
-          flying out of <input type="text" name="origin_city_iata" placeholder="NYC" />
-          <br></br>
-          <button onClick={this.props.callFlightAPI}>Let's Go!</button>
+        <h3>Destination Search</h3>
+        <form className="destination-search search" onSubmit={this.props.handleSubmit}>
+          <div>I want a vaction to <input type="text" name="origin_city_iata" placeholder="LON" />
+          </div>
+          <div>for <input type="number" name="num_people" placeholder="2" /> people
+          </div>
+          <div>flying out of <input type="text" name="origin_city_iata" placeholder="NYC" />
+          </div>
+          <button>Let's Go!</button>
         </form>
       </div>
     );
--- frontent-clicktrip/src/ManualSearch.js
+++ frontent-clicktrip/src/ManualSearch.js
@@ -4,16 +4,17 @@ function ManualSearch(props) {
   return (
 
     <div className="manual-search search">
-      <form onSubmit={props.handleSubmit}>
-        I want a vaction for <input type="number" name="num_people" placeholder="2" /> people
-        <br></br>
-        for under $<input type="number" name="budget" placeholder="1200" />
-        <br></br>
-        from <input type="date" name="start_date" /> to <input type="date" name="end_date" />
-        <br></br>
-        flying out of <input type="text" name="origin_city_iata" placeholder="NYC" />
-        <br></br>
-        <button onClick={props.callPriceFlightAPI}>Let's look at flights!</button>
+      <h3>Search By Price</h3>
+      <form className="manual-search search" onSubmit={props.handleSubmit}>
+        <div>I want a vaction for <input type="number" name="num_people" placeholder="2" /> people
+        </div>
+        <div>for under $<input type="number" name="budget" placeholder="1200" />
+        </div>
+        <div>from <input type="date" name="start_date" /> to <input type="date" name="end_date" />
+        </div>
+        <div>flying out of <input type="text" name="origin_city_iata" placeholder="NYC" />
+        </div>
+        <button>Let's look at flights!</button>
       </form>
     </div>
   );
